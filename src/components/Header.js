0b42d24360fa35a025662860db0614a3f338d7e8
Benedict <newlyf38@gmail.com>
2023-09-18 11:34:46 +0000

    styled my container in the header.js file(component)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,24 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <div>
-      Header
-    </div>
+    <Container>
+        <Content>
+            Header
+        </Content>
+    </Container>
   )
 }
+
+const Container = styled.div`
+    background-color: white;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+    left: 0;
+    padding: 0px 24px;
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    z-index: 100;
+`;
+const Content = styled.div``;
 
 export default Header
